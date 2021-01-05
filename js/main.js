460e0482c2298c745ac4a642fada9a597ffbd605
Bartosz Nocun <bartosznocun1@gmail.com>
2021-01-05 18:58:19 +0100

    Create portfolio section and add slick.js

--- js/main.js
+++ js/main.js
@@ -5,4 +5,11 @@ const handleClick = () => {
     hamburger.classList.toggle('hamburger--active');
     mobileNav.classList.toggle('mobile-navbar--show');
 }
-hamburger.addEventListener('click', handleClick)
+hamburger.addEventListener('click', handleClick);
+
+//slickJS
+$(document).ready(function () {
+    $('.your-class').slick({
+        // setting - name: setting - value
+    });
+});
